934d9e2aff665b89059a6c14b9254548a0f018fb
jxixeun <jxixeun@gmail.com>
2022-09-13 15:13:17 +0900

    Initial commit

--- src/main/java/com/example/appserver/service/MemberService.java
+++ src/main/java/com/example/appserver/service/MemberService.java
@@ -5,6 +5,9 @@ import com.example.appserver.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MemberService {
 
@@ -27,6 +30,17 @@ public class MemberService {
      * 중복 회원 검증
      */
     private void validateDuplicateMember(Member member) {
-
+        memberRepository.findByEmail(member.getEmailAddress())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 이메일입니다.");
+                });
     }
+
+    /**
+     * 전체 회원 조회
+     */
+    private List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
 }
