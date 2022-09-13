fe7be26f1eb0f2dea8bffaadd04398643718127b
shkum0330 <shkum0330@gmail.com>
2022-09-13 14:59:24 +0900

    test

--- src/main/java/com/example/appserver/domain/Member.java
+++ src/main/java/com/example/appserver/domain/Member.java
@@ -1,9 +1,11 @@
 package com.example.appserver.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@RequiredArgsConstructor
 public class Member {
     private Long id;
     private String username;
