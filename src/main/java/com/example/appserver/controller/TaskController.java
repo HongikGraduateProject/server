5b073e95049c1ecc68ab3fbe2c833e6fd978a6c7
jxixeun <jxixeun@gmail.com>
2022-09-13 15:55:08 +0900

    task modified

--- src/main/java/com/example/appserver/controller/TaskController.java
+++ src/main/java/com/example/appserver/controller/TaskController.java
@@ -4,9 +4,11 @@ import com.example.appserver.domain.Task;
 import com.example.appserver.repository.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,10 +18,12 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping("/tasktest")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/task/save")
     public String taskSave(@RequestBody Task task) {
         log.info("username={}, age={}", task.getId(), task.getContents());
         taskRepository.save(task.getId(), task.getContents());
         return "저장완료";
     }
+
 }
