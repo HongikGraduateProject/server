f10ed2337803bd5f2c3b220c4f1319d6e1984338
jxixeun <jxixeun@gmail.com>
2022-09-13 15:14:01 +0900

    Initial commit

--- src/main/java/com/example/appserver/controller/MemberController.java
+++ src/main/java/com/example/appserver/controller/MemberController.java
@@ -1,4 +1,13 @@
 package com.example.appserver.controller;
 
+import com.example.appserver.repository.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
+
+    private MemberRepository memberRepository;
+
+
 }
