300037eb3ba69aa00a95fbe248dfbedbfa0e6c65
jxixeun <jxixeun@gmail.com>
2022-09-13 15:53:14 +0900

    taskSave

--- src/main/java/com/example/appserver/controller/TaskController.java
+++ src/main/java/com/example/appserver/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.example.appserver.controller;
 import com.example.appserver.domain.Task;
 import com.example.appserver.repository.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
 
+    @Autowired
     private TaskRepository taskRepository;
 
     @PostMapping("/tasktest")
-    public void taskSave(@RequestBody Long userId, @RequestBody String contents) {
-        log.info("username={}, age={}", userId, contents);
-        //taskRepository.save(task.getId(), task);
+    public String taskSave(@RequestBody Task task) {
+        log.info("username={}, age={}", task.getId(), task.getContents());
+        taskRepository.save(task.getId(), task.getContents());
+        return "저장완료";
     }
 }
